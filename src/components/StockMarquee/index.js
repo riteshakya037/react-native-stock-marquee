9b643c5b7568f8ccf4dc7de75c9d59e88e2c503f
Ritesh Shakya <riteshakya037@gmail.com>
2020-04-11 15:33:24 +0545

    - Added animated scrolling

--- src/components/StockMarquee/index.js
+++ src/components/StockMarquee/index.js
@@ -7,6 +7,13 @@ const NO_PER_SCREEN = 5;
 const itemWidth = Dimensions.get('window').width / NO_PER_SCREEN;
 
 class StockMarquee extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPosition: 0,
+    };
+  }
+
   _renderItem = (item, index) => {
     return (
       <MarqueeItem
@@ -22,10 +29,50 @@ class StockMarquee extends Component {
     );
   };
 
+  componentDidMount() {
+    this.startScroll();
+  }
+
+  // Clear interval when user closes
+  componentWillUnmount() {
+    clearInterval(this.activeInterval);
+  }
+
+  startScroll() {
+    this.activeInterval = setInterval(this.scrolling, 32);
+  }
+
+  scrolling = () => {
+    // Start scrolling if there's more than one stock to display
+    const {data} = this.props;
+    const {currentPosition} = this.state;
+
+    if (data.length > NO_PER_SCREEN) {
+      // Increment position with each new interval
+      const position = currentPosition + 0.5;
+      this.ticker.scrollToOffset({offset: position, animated: false});
+      const maxOffset = data.length * itemWidth;
+      // Set animation to repeat at end of scroll
+      if (currentPosition > maxOffset) {
+        const offset = 0;
+        this.ticker.scrollToOffset({
+          offset,
+          animated: false,
+        });
+        this.setState({currentPosition: offset});
+      } else {
+        this.setState({currentPosition: position});
+      }
+    }
+  };
+
   render() {
     const {data} = this.props;
     return (
       <FlatList
+        ref={(ref) => {
+          this.ticker = ref;
+        }}
         getItemLayout={(_, index) => ({
           length: data.length,
           offset: itemWidth * index,
